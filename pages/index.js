ada66090eb6bb529ad44ae8d20970e94fb0b6072
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-01-25 14:30:21 -0300

    Configuração da index para buscar usuários.

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from "react";
+import { useRouter } from 'next/router'
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
 
 function Titulo (props) {
     const Tag = props.tag
@@ -48,11 +23,11 @@ function Titulo (props) {
 }
 
 export default function PaginaInicial() {
-    const username = 'thaissilvr';
-  
+    const [username, setUsername] = React.useState('thaissilvr')
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -79,6 +54,11 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (event){
+                event.preventDefault()
+                console.log('alguém enviou o form')
+                roteamento.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -90,6 +70,11 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value = {username}
+                onChange={function (event){
+                  const userValue = event.target.value
+                  setUsername(userValue)
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
